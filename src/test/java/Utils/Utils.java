2eb6166ba480a32b46a606abf060e6abf2cdc53c
macar <marina.cboghossian@gmail.com>
2024-01-16 15:14:27 -0300

    inclusão do metodo selecionarCombo na classe Utils

--- src/test/java/Utils/Utils.java
+++ src/test/java/Utils/Utils.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.Select;
 
 import javax.swing.*;
 
@@ -47,6 +48,16 @@ driver = new ChromeDriver();
 	driver.get(url);
 	driver.manage().window().maximize();
 
+	//comando para posicionar a tela (caso estiver trabalhando em duas telas e quiser que o codigo rode na outra tela.
+	//OBS: substituir o .maximize pelo código abaixo, os dois juntos não irá funcionar
+	// driver.manage().window().setPosition(newPoint(x,y)
+	//no x, y colocar a posição da tela desejada. Ex: 100,100
+
+	//comando para definir o tamanho da tela
+	// driver.manage().window().setSize(newDimension(x,y)
+
+	//driver.close()  - fechar apenas uma aba do browser
+
 }
 
 public void validarTexto(By elemento, String texto) {
@@ -81,6 +92,23 @@ String TextoTela = driver.findElement(elemento).getText();
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 		jse.executeScript("window.scrollBy(0,300)");
 	}
+
+	public void selecionarCombo(By elemento, String valor){
+		//método para selecionar informações quando há um combobox com várias opções
+		WebElement element = driver.findElement(elemento);
+		Select combo = new Select(element);
+		combo.selectByVisibleText(valor);
+
+		//posso utilizar também a seleção por Index que é pela ordem da listagem de opções do combobox ou pelo value do inspecionar
+		//combo.selectByIndex(valor);
+		//combo.selectByValue(valor);
+
+	}
+
+
+// atalhos adicionais
+	// pular linha usar comando /n
+
 
 
 }
